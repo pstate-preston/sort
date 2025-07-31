88e7572ecf8327261a2619afa780736d43a5f865
pstate-preston <gautreaux.t.preston@gmail.com>
2025-07-31 16:40:51 -0500

    resolved benchmark

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,11 +3,15 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use rand::Rng;
 
 use Sort::merge_sort::merge_sort;
-// use Sort::heap_sort::heap_sort;
+use Sort::heap_sort::heap_sort;
 use Sort::selection_sort::selection_sort;
 use Sort::bubble_sort::bubble_sort;
 
-// generates random vector of chosen size(range 0-1000) to pass as test.
+/// generates random vector of chosen size(range 0-1000) to pass as test.
+/// # Parameter
+/// * length - the desired length of the vector
+/// # Returns
+/// * returns a Vec<i32> of the specified length
 fn random_vector(length: i32) -> Vec<i32> {
     let mut rng = rand::rng();
     let mut elements: Vec<i32> = vec![];
@@ -17,21 +21,23 @@ fn random_vector(length: i32) -> Vec<i32> {
     elements
 }
 
-// Problem: Heap sort is an in-place algorithm. Maybe need to adjust it to also take an array,
-// then return a vector like the others? This may not be the best way to implement, but to
-// compare the raw efficiency of heap and merge it may be useful.
 fn sort_benchmark(c: &mut Criterion){
     
-    let vector = random_vector(10000);
-    let test: &[i32] = &vector;
-    //println!("{:?}", test);
+    let mut vector = random_vector(10000);
+    let mut test_inplace = vector.clone();
+    let test: &[i32] = &mut vector;
     println!("\n\n\n\n");
 
-    //let test = [9, 5, 3, 9, 9, 8, 2, 4, 4, 4, 26, 29, 30, 43, 91, 10025];
     c.bench_function(
         "Merge Sort",
         |b|
             b.iter(|| merge_sort(black_box(test)))
+    );
+
+    c.bench_function(
+        "Heap Sort",
+        |b|
+            b.iter(|| heap_sort(black_box(&mut test_inplace)))
     );
 
     c.bench_function(
