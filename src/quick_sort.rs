0ce43876407b45b0443b53c56c532525f9282622
pstate-preston <gautreaux.t.preston@gmail.com>
2025-08-01 17:09:06 -0500

    added quick_sort module, no tests yet

---
+++ src/quick_sort.rs
@@ -0,0 +1,29 @@
+/// Time complexity is O(n log(n)) best case, O(n^2) worst case.
+/// # Parameters
+/// * data: &mut Vec<[T]> - a reference to vector containing numerical data.
+/// # Returns
+/// * Vec<[T]> - the original parameter array, sorted in place.
+pub fn quick_sort<T: Copy + Ord>(data: &mut Vec<T>) {
+    let n = data.len();
+    let pivot = n - 1;
+    let (mut i, mut j) = (0, pivot-1);
+
+    while i < j {
+        if data[i] > data[pivot] && data[j] < data[pivot] {
+            data.swap(i, j);
+            i += 1;
+            j -= 1;
+        } else if data[i] > data[pivot] {
+            j -= 1;
+        } else {
+            i += 1;
+        }
+    }
+    // now, i and j have crossed over partition...
+    data.swap(i, pivot);
+    let mut left: Vec<T> = data[..pivot].to_vec();
+    let mut right: Vec<T> = data[pivot+1..].to_vec();
+
+    quick_sort(&mut left);
+    quick_sort(&mut right);
+}
